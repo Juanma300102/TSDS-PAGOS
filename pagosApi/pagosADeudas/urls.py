cb4ca67d159d19ad19503407320911f1da282e61
Juanma <pedrozo.juanma@gmail.com>
2022-10-17 19:20:49 -0300

    fix router registration

--- pagosApi/pagosADeudas/urls.py
+++ pagosApi/pagosADeudas/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import SimpleRouter
 from pagosADeudas.views import PagosADeudasViewSet
 
 router = SimpleRouter()
-router.register("servicios", PagosADeudasViewSet)
+router.register("pagos-a-deudas", PagosADeudasViewSet)
 
 urlpatterns = router.urls
