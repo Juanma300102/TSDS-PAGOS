36571bdb1db7125e3dcd8edc56563d97b1713a11
Juanma <pedrozo.juanma@gmail.com>
2022-10-17 19:54:48 -0300

    fix typo

--- pagosApi/pagosApi/router.py
+++ pagosApi/pagosApi/router.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", include(("bonificaciones.urls", "bonificaciones"), "bonificaciones")),
     path("", include(("deudas.urls", "deudas"), "deudas")),
     path("", include(("personas.urls", "personas"), "personas")),
-    path("", include(("periodos.url", "periodos"), "periodos")),
+    path("", include(("periodos.urls", "periodos"), "periodos")),
     path("", include(("servicios.urls", "servicios"), "servicios")),
     path("", include(("montosBase.urls", "montosBase"), "montosBase")),
     path("", include(("pagosADeudas.urls", "pagosADeudas"), "pagosADeudas")),
